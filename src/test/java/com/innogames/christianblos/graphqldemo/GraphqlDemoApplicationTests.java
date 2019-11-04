92f0f882bb63e174cf1cf9c77b5d21125e64e6c0
Christian Blos <christian.blos@innogames.com>
2019-11-04 19:58:04 +0100

    Spring Initializr

---
+++ src/test/java/com/innogames/christianblos/graphqldemo/GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innogames.christianblos.graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
